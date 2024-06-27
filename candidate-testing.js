678d17b46d5ef1a5b97bb5715d5ba35eb046bc70
Katherine Hanrahan <katherinehanrahan@Corins-MBP.lan>
2024-06-27 17:34:15 -0500

    Graded Assignment #1 - Part 2 In Progress

--- candidate-testing.js
+++ candidate-testing.js
@@ -10,9 +10,9 @@ let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? " , "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"]
+let candidateAnswers = [];
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
@@ -23,6 +23,13 @@ function askForName() {
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
   candidateAnswer = input.question(question);
+  //candidateAnswers = input.questions(questions);
+
+  
+  for (let i = 0; i < questions.length; i++) {
+    candidateAnswers[i] = input.questions(questions[i]); 
+    candidateAnswers.push(input.questions(questions[i]));
+  } 
 }
 
 function gradeQuiz(candidateAnswers) {
@@ -45,6 +52,8 @@ function runProgram() {
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
+
+
 
 // ----------- Don't write any code or change any code below this line ---------- //
 module.exports = {
